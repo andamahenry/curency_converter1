2344e3f99b00b1bc4aa41fc6580e6a7d31b257b6
andamahenry <33576566+andamahenry@users.noreply.github.com>
2019-04-24 11:01:55 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 function currency_query(){
 	var display_span = document.getElementById("current_rate");
-	alert('Lindirira!');
 	display_span.innerHTML = 'Waiting for server response, now...';
 	var req = new XMLHttpRequest();
 	req.open('POST','http://jerm.000webhostapp.com',true);
@@ -11,8 +10,9 @@ function currency_query(){
 			var server_response = this.responseText;
 			var data = JSON.parse(server_response);
 			//for(var item in data){
-				console.log("USD_UGX"+"==>"+data['USD_UGX']);
-				display_span.innerHTML = "USD_UGX"+"==>"+data["USD_UGX"];
+			alert('Lindirira!');
+			console.log("USD_UGX"+"==>"+data['USD_UGX']);
+			display_span.innerHTML = "USD_UGX"+"==>"+data["USD_UGX"];
 			//}
 		}else{
 			alert(this.status+"==>"+this.responseText);
