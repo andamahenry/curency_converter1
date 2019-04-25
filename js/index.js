bd35a932056e23c7840b3c4fef7ab8509e4f626b
glayn2bukman <glayn2bukman@gmail.com>
2019-04-25 13:11:30 +0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,25 +1,36 @@
+var URLs={
+    rate:{
+        mtd: 'POST', url:'https://jerm.000webhostapp.com',
+},};
+
+var DISPLAY_SPAN;
+
 function currency_query(){
-	var display_span = document.getElementById("current_rate");
-	display_span.innerHTML = 'Waiting for server response, now...';
+	DISPLAY_SPAN.innerHTML = 'Waiting for server response, now...';
 	var req = new XMLHttpRequest();
-	req.open('POST','https://jerm.000webhostapp.com',true);
+	req.open(URLs.rate.mtd,URLs.rate.url,true);
 	
-	//req.onload = function (){
 	req.onreadystatechange = function (){
-		if(this.status==200 || this.readyState==4){
-			var server_response = this.responseText;
-			var data = JSON.parse(server_response);
-			//for(var item in data){
-			console.log("USD_UGX"+"==>"+data['USD_UGX']);
-			display_span.innerHTML = "USD_UGX"+"==>"+data["USD_UGX"];
-			alert('Lindirira!');
-			//}
-		}else{
-			alert(this.status+"==>"+this.responseText);
-		}
-		
+        if(this.readyState == 4){
+            if(this.status===200){ // notice i use === instead of ==
+                var data = JSON.parse(this.responseText);
+                console.log("USD_UGX"+"==>"+data['USD_UGX']);
+                DISPLAY_SPAN.innerHTML = "USD_UGX"+"==>"+data["USD_UGX"];
+                alert('Lindirira!');
+            }else if(!this.status){
+                DISPLAY_SPAN.innerHTML = "URL ["+URLs.rate.url+"] does not exist";
+            }
+            else{
+                DISPLAY_SPAN.innerHTML = "ERROR: "+this.status+"<br>"+"URL:"+this.responseURL;
+                alert(this.status+"==>"+this.responseText);
+            }
+        }
 	}
 	var payload = new FormData();
 	payload.append('convert_dollar','');
 	req.send(payload);
+}
+
+window.onload = function(){
+    DISPLAY_SPAN = document.getElementById("current_rate");
 }
