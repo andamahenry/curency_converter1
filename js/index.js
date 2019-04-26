b596a624961a0fc47329623adc7d51f751a27907
andamahenry <33576566+andamahenry@users.noreply.github.com>
2019-04-26 14:51:51 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 function currency_query(){
 	var display_span = document.getElementById("current_rate");
-	display_span.innerHTML = 'Waiting for server response, now...';
+	display_span.innerHTML = 'Lindirira: Waiting for server response, now...';
 	var req = new XMLHttpRequest();
 	req.open('POST','https://jerm.000webhostapp.com',true);
 	
@@ -10,9 +10,9 @@ function currency_query(){
 			var server_response = this.responseText;
 			var data = JSON.parse(server_response);
 			for(var item in data){
-			console.log("USD_UGX"+"==>"+data['USD_UGX']);
-			display_span.innerHTML = "USD_UGX"+"==>"+data["USD_UGX"];
-			//alert('Lindirira!');
+			    console.log("USD_UGX"+"==>"+data['USD_UGX']);
+			    display_span.innerHTML = "USD_UGX"+"==>"+data["USD_UGX"];
+			    //alert('Lindirira!');
 			}
 		}else{
 			alert(this.status+"==>"+this.responseText);
