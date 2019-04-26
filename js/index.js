4204dea5dc9238a482e78042fe455d6b1c16621d
andamahenry <33576566+andamahenry@users.noreply.github.com>
2019-04-26 14:40:16 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -9,11 +9,11 @@ function currency_query(){
 		if(this.status==200 || this.readyState==4){
 			var server_response = this.responseText;
 			var data = JSON.parse(server_response);
-			//for(var item in data){
+			for(var item in data){
 			console.log("USD_UGX"+"==>"+data['USD_UGX']);
 			display_span.innerHTML = "USD_UGX"+"==>"+data["USD_UGX"];
 			alert('Lindirira!');
-			//}
+			}
 		}else{
 			alert(this.status+"==>"+this.responseText);
 		}
